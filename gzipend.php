189fe92b428b8387d341b1a26f617d1375e0e8b8
matttolman <tolmlanmatt@gmail.com>
2016-09-22 08:57:59 -0600

    Removed debug code

--- gzipend.php
+++ gzipend.php
@@ -1,6 +1,6 @@
 <?php
 $str = ob_get_clean();
-//$str = preg_replace("/\s\s+/"," ",$str);
+$str = preg_replace("/\s\s+/"," ",$str);
 if(!ob_start("ob_gzhandler")) ob_start();
 echo $str;
 ob_flush();
